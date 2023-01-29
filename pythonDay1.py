5454bf508ee6d6c5183111c2392b9f2ce6cbf5ec
TahoeBoelat <muhammadnuralfi98@gmail.com>
2023-01-29 18:39:19 +0700

    3

--- pythonDay1.py
+++ pythonDay1.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 # First python exercise 
 # Source for this exercise: https://www.w3resource.com/python-exercises/python-basic-exercises.php
 # Write a Python program to print the following string in a specific format 
@@ -6,3 +7,6 @@ omagad = 'Twinkle, twinkle little star, \n\t How i wonder what you are! \n\t\t U
 print(omagad)
 # Write a Python program to find out what version of Python you are using.
 print(sys.version)
+# Write a Python program to display the current date and time.
+tanggaldanwaktu = datetime.datetime.now()
+print(tanggaldanwaktu.strftime("%Y-%m-%d %H:%M:%S"))
