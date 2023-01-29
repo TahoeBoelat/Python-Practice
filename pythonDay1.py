41ffeb50f253e1a868a2151ce313f8cbca8d9f92
TahoeBoelat <muhammadnuralfi98@gmail.com>
2023-01-29 18:21:03 +0700

    Day 1

---
+++ pythonDay1.py
@@ -0,0 +1,8 @@
+import sys
+# First python exercise 
+# Source for this exercise: https://www.w3resource.com/python-exercises/python-basic-exercises.php
+# Write a Python program to print the following string in a specific format 
+omagad = 'Twinkle, twinkle little star, \n\t How i wonder what you are! \n\t\t Up above the world so high, \n\t\t Like a Diamond in the sky. \n Twinkle-twinkle little star, \n\t How i wonder what you are'
+print(omagad)
+# Write a Python program to find out what version of Python you are using.
+print(sys.version)
